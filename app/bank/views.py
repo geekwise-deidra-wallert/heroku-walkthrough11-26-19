6dca936fe8eab4bdf2f71b97903886dffaa40e98
geekwise-deidra-wallert <geekwise.deidra.wallert@gmail.com>
2019-11-26 22:56:32 +0000

    creating views and such for rest framework

---
+++ app/bank/views.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from bank.serializers import UserSerializer, GroupSerializer
+
+class UserViewSet ( viewsets.ReadOnlyModelViewSet ):
+    """
+        API endpoint that allows users to be viewed or edited
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class GroupViewSet ( viewsets.ReadOnlyModelViewSet ):
+    """
+        API endpoint that allows users to be viewed or edited
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+© 2019 GitHub, Inc.
